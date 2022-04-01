df629e7448f41044c2f446ce0f66a746d8410319
JiyeonP <66158942+JiyeonP@users.noreply.github.com>
2022-04-02 00:19:05 +0900

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# I want to make sure that the directory is clean and has nothing left over from
-# previous deployments. The servers auto scale so the directory may or may not
-# exist.
-
 if [ -d /home/ubuntu/interactive-web-client/ ]; then
     rm -rf /home/ubuntu/interactive-web-client/
 fi
