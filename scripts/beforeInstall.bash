1ba9a51a26c31f670ac58bf265e52393350f9059
JiyeonP <66158942+JiyeonP@users.noreply.github.com>
2022-04-01 20:57:32 +0900

    Create beforeInstall.bash

---
+++ scripts/beforeInstall.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# I want to make sure that the directory is clean and has nothing left over from
+# previous deployments. The servers auto scale so the directory may or may not
+# exist.
+
+if [ -d /home/ubuntu/interactive-web-client/ ]; then
+    rm -rf /home/ubuntu/interactive-web-client/
+fi
+mkdir -vp /home/ubuntu/interactive-web-client/
