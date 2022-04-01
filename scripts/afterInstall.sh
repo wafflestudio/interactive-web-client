45505a4a1ace41cbfdc1f4eaa1a99f16170f41ab
JiyeonP <66158942+JiyeonP@users.noreply.github.com>
2022-04-02 01:19:40 +0900

    Update afterInstall.sh

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,9 +1,7 @@
-source /home/ubuntu/.profile
+#source /home/ubuntu/.profile
 
 #REPOSITORY=/home/ubuntu/interactive-web-client
 #cd $REPOSITORY
-
-/usr/local/bin/pm2 start npm --name "admin" -- start
 
 #pm2 start npm --name "next" -w -- start
 #pm2 save
