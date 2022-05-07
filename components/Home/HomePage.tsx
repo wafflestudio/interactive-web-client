1e608861e6659dc6c3be7803463df9d196bddc6d
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-05-07 22:10:02 +0900

    feat: connect getMe API in main page

--- components/Home/HomePage.tsx
+++ components/Home/HomePage.tsx
@@ -1,9 +1,28 @@
-import HomeMain from './Main/HomeMain'
-import PageHead from './PageHead'
-import SideBar from './Sidebar/SideBar'
-import styles from './HomePage.module.scss'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { api } from "../../api/api";
+import { setUser } from "../../modules/user";
+import HomeMain from "./Main/HomeMain";
+import PageHead from "./PageHead";
+import SideBar from "./Sidebar/SideBar";
+import styles from "./HomePage.module.scss";
 
 export default function HomePage() {
+  const dispatch = useDispatch();
+
+  const checkLogin = async () => {
+    try {
+      const { data } = await api._getme();
+      dispatch(setUser({ ...data, isLoggedIn: true }));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
   return (
     <>
       <PageHead />
@@ -14,5 +33,5 @@ export default function HomePage() {
         </div>
       </div>
     </>
-  )
+  );
 }
---
+++ pages/user/index.tsx
@@ -0,0 +1,5 @@
+const User = () => {
+  return <div></div>;
+};
+
+export default User;
