10a3d8c62ada84d03cf91179b93d015eb62e7e0b
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-05-07 18:57:54 +0900

    feat:links underline when mouse hover

--- components/Home/Sidebar/SideBar.tsx
+++ components/Home/Sidebar/SideBar.tsx
@@ -1,13 +1,16 @@
-import Navigator from './Navigator'
-import UserInfo from './UserInfo'
-import styles from './SideBar.module.scss'
+import Link from "next/link";
+import Navigator from "./Navigator";
+import UserInfo from "./UserInfo";
+import styles from "./SideBar.module.scss";
 
 export default function SideBar() {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>로고</div>
+      <div className={styles.logo}>
+        <Link href="/">로고</Link>
+      </div>
       <UserInfo />
       <Navigator />
     </div>
-  )
+  );
 }
